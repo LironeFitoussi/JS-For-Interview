005ee474e04f066efe3e9c783dcf258c763231a8
LironeFitoussi <95374011+LILo2610@users.noreply.github.com>
2024-03-12 22:19:28 +0200

    3

---
+++ Answers/3. Hoisting/main.js
@@ -0,0 +1,14 @@
+// What will be console.logged here
+
+// Question 1
+console.log(foo); // Output: Error: foo is undefined
+foo = 1;
+
+// question 2
+console.log(foo); // Output: 2
+var foo = 2;
+
+// question 3
+foo = 3;
+console.log(foo); // Output: 3
+var foo;
